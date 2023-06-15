8fd04a86968c8c9b53961a12a5079de0e02d2aa8
Chaos <tinywonk@yahoo.com>
2023-06-14 22:03:22 -0400

    Added yaml file

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://chaosandvideogames.netlify.app',
+  site: 'https://chaosandvideogames.com',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
   }), node({
