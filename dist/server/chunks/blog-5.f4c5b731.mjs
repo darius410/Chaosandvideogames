e74b1dfc932d73ab276e4227b2c5c44594d20f2b
Chaos <tinywonk@yahoo.com>
2023-06-17 12:05:57 -0400

    trigger rebuild

---
+++ dist/server/chunks/blog-5.f4c5b731.mjs
@@ -0,0 +1,8 @@
+async function getMod() {
+						return import('./blog-5.6a7b2ec8.mjs');
+					}
+					const collectedLinks = ["/_astro/global.1adfc04e.css","/_astro/lg.f96e1f42.css","/_astro/med.5e1c0096.css","/_astro/reset.e49af181.css","/_astro/sm.bb36cbd6.css"];
+					const collectedStyles = "@@ASTRO-STYLES@@";
+					const collectedScripts = [{"props":{"src":"/_astro/Navigation.astro_astro_type_script_index_0_lang.a0756a62.js","type":"module"},"children":""}];
+
+export { collectedLinks, collectedScripts, collectedStyles, getMod };
