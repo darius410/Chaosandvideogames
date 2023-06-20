09ec42ec9163da54675ead29d22b0dcee7cd0e25
Chaos <tinywonk@yahoo.com>
2023-06-20 14:16:26 -0400

    Updates

--- dist/server/chunks/blog-9.260fa563.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./blog-9.b38938d0.mjs');
-					}
-					const collectedLinks = ["/_astro/global.1adfc04e.css","/_astro/lg.f96e1f42.css","/_astro/med.5e1c0096.css","/_astro/reset.e49af181.css","/_astro/sm.bb36cbd6.css"];
-					const collectedStyles = "@@ASTRO-STYLES@@";
-					const collectedScripts = [{"props":{"src":"/_astro/Navigation.astro_astro_type_script_index_0_lang.a0756a62.js","type":"module"},"children":""}];
-
-export { collectedLinks, collectedScripts, collectedStyles, getMod };
