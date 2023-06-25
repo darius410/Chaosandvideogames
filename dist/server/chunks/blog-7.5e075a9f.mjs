2512f0fb7269ddd5975392a74c516f5abdbf9472
Chaos <tinywonk@yahoo.com>
2023-06-24 23:01:51 -0400

    Trigger biuld

---
+++ dist/server/chunks/blog-7.5e075a9f.mjs
@@ -0,0 +1,8 @@
+async function getMod() {
+						return import('./blog-7.23d71473.mjs');
+					}
+					const collectedLinks = ["/Chaosandvideogames/assets/global.1adfc04e.css","/Chaosandvideogames/assets/lg.4c130b0e.css","/Chaosandvideogames/assets/med.cb03cc5c.css","/Chaosandvideogames/assets/reset.e49af181.css","/Chaosandvideogames/assets/sm.eb6e3ad8.css"];
+					const collectedStyles = "@@ASTRO-STYLES@@";
+					const collectedScripts = [{"props":{"src":"/Chaosandvideogames/assets/Navigation.astro_astro_type_script_index_0_lang.a0756a62.js","type":"module"},"children":""}];
+
+export { collectedLinks, collectedScripts, collectedStyles, getMod };
