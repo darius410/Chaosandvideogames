b29f7f9cad209902b81f1d7c8b6480f372455015
Chaos <tinywonk@yahoo.com>
2023-07-07 18:23:09 -0400

    new trigger

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,29 +2,29 @@ import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
 // import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
-import netlify from '@astrojs/netlify/static';
+// import netlify from '@astrojs/netlify/static';
 export default defineConfig({
   site: 'https://chaosandvideogames.com',
-  debug: true,
-  output: "server",
-  adapter: netlify(),
-  experimental: {
-    redirects: true,
-  },
+  // debug: true,
+  // output: "server",
+  // adapter: netlify(),
+  // experimental: {
+  //   redirects: true,
+  // },
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
   }), sitemap()],
   
   // base: 'Chaosandvideogames',
-    build:{
-      format: 'file',
+  //   build:{
+  //     format: 'file',
      
-      // assetsPrefix: 'https://Chaosandvideogames.com',
-     assets:'assets',     //Puts all CSS and JS files  in here - Ex ./dist/client/valueGiven
-    // client: './Chaosandvideogames' ,   //Makes dist folder based on the value given
-    //  server:'./server'
-  },
-  trailingSlash: "always",
+  //     // assetsPrefix: 'https://Chaosandvideogames.com',
+  //    assets:'assets',     //Puts all CSS and JS files  in here - Ex ./dist/client/valueGiven
+  //   // client: './Chaosandvideogames' ,   //Makes dist folder based on the value given
+  //   //  server:'./server'
+  // },
+  // trailingSlash: "always",
  
 
 });
