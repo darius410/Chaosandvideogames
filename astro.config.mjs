a297e6980ba68ac5896d51dfe2666c593165f3b7
Chaos <tinywonk@yahoo.com>
2023-07-07 17:35:17 -0400

    trigger build removed astro config settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,24 @@ import netlify from '@astrojs/netlify/functions';
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chaosandvideogames.com',
-  output: 'server',
-  adapter: netlify(),
-  debug: true,
+  // output: 'server',
+  // adapter: netlify(),
+  // debug: true,
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap()],
-  // base: 'Chaosandvideogames',
-  build: {
-    format: 'directory',
-    // assetsPrefix: 'https://Chaosandvideogames.com',
-    assets: 'assets' //Puts all CSS and JS files  in here - Ex ./dist/client/valueGiven
-    // client: './Chaosandvideogames' ,   //Makes dist folder based on the value given
-    //  server:'./server'
-  },
+  }), sitemap()]
+  // // base: 'Chaosandvideogames',
+  // build: {
+  //   format: 'directory',
+  //   // assetsPrefix: 'https://Chaosandvideogames.com',
+  //   assets: 'assets' //Puts all CSS and JS files  in here - Ex ./dist/client/valueGiven
+  //   // client: './Chaosandvideogames' ,   //Makes dist folder based on the value given
+  //   //  server:'./server'
+  // },
 
-  trailingSlash: "always"
+  // trailingSlash: "always"
 });
